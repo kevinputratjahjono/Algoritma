1c0df56269e28e86ae22d467a4592819f081aef1
Kevin Putra Tjahjono <91048002+kevinputratjahjono@users.noreply.github.com>
2021-09-27 00:11:08 +0700

    Latihan 2

---
+++ luas.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 24 12:11:03 2021
+
+@author: kevin putra tjahjono
+NIM: 065.002.100.020
+"""
+
+print("MENGHITUNG LUAS RUANGAN")
+
+lebar = float(input("Masukan Lebar: "))
+panjang = float(input("Masukan Panjang: "))
+luas = panjang*lebar
+satuan = input("Meter/inci: ")
+
+        
+print("Luas Ruangan dengan panjang",panjang, "dan lebar", lebar, "adalah", luas, satuan)
