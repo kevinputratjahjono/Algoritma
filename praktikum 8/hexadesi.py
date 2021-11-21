453c68e98a08f198255e4a13657391cf78120ca6
Kevin Putra Tjahjono <91048002+kevinputratjahjono@users.noreply.github.com>
2021-11-22 00:44:19 +0700

    hexa ke desi sebaliknya

---
+++ praktikum 8/hexadesi.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 15 14:43:14 2021
+
+@author: vinix
+"""
+print ("Program konversi Desimal, Hexadecimal")
+print ("Angka Hexa diawali 0x")
+print ("Pilih [1] untuk Desimal ke Hexa")
+print ("Pilih [2] untuk Hexa ke Desimal")
+"""
+i = int(input("Masukkan pilihan anda: "))
+
+def deshexa(x):
+   if x == 0:
+      return (0x0)
+      print(x)
+   else:
+      return (hex(x))
+      print(x)
+def hexades(x):
+    if x == 0:
+        return 0
+        print(x)
+    else:
+          return (x,16)
+          print(x)
+
+x = int(input("masukkan angka Desimal: "))
+"""
+i = int(input("Masukkan pilihan anda: "))
+
+if i == 1:
+    x = int(input("masukkan angka Desimal: "))
+    print (hex(x))
+
+elif i == 2:
+    x = int(input("masukkan angka Hexa: "),16)
+    print (x)
+
+else:
+    print("Pilihan anda salah");
+       
