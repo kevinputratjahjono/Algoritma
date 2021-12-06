d38e66c5726367a38f7589b13176a4ab368ee0cf
Kevin Putra Tjahjono <91048002+kevinputratjahjono@users.noreply.github.com>
2021-12-06 14:00:03 +0700

    nambahin doang

--- Laporan 10/rekursifbubble.py
+++ Laporan 10/rekursifbubble.py
@@ -5,11 +5,17 @@ Created on Mon Dec  6 13:28:58 2021
 @author: vinix
 """
 
+def tukar(A, i, j):
+ 
+    temp = A[i]
+    A[i] = A[j]
+    A[j] = temp
+ 
 def bubbleSort(A, n):
  
     for i in range(n - 1):
         if A[i] > A[i + 1]:
-            A[i], A[i+1] = A[i+1], A[i]
+            tukar(A, i, i + 1)
  
     if n - 1 > 1:
         bubbleSort(A, n - 1)
